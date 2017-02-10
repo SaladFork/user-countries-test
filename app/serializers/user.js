857fd55bdea986fa25e3956cd3a49f91f20a1cf1
Elad Shahar <SaladFork@Gmail.com>
2017-02-10 11:41:51 -0500

    Add countries link to user serializer

--- app/serializers/user.js
+++ app/serializers/user.js
@@ -1,8 +1,23 @@
+import Ember from 'ember';
 import DS from 'ember-data';
+
+const {
+  getWithDefault
+} = Ember;
 
 const {
   RESTSerializer
 } = DS;
 
 export default RESTSerializer.extend({
+  normalize(modelClass, resourceHash) {
+    let links = getWithDefault(resourceHash, 'links', {});
+
+    // FIXME: Can we use the Countries serializer to get this instead of hard
+    // coding the path?
+    links.countries = '/countries';
+    resourceHash.links = links;
+
+    return this._super(modelClass, resourceHash);
+  }
 });
